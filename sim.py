5164f72194cf055b4b893266fc230c1e314515e5
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-06-13 19:19:07 -0400

    Python noob struggles with lack of semicolons

--- sim.py
+++ sim.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+import random
+from random import randint
+
+class Target:
+    def __init__(self):
+        self.x = random.randint(0,500)
+        self.y = random.randint(0,500)
+
+targets = []
+
+for i in range(17):
+    targets.append(Target())
+
+for node in targets:
+    print(node.x, ", ", node.y);

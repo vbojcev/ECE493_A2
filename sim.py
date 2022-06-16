10e790dc85e79275772c6ef0019a698a5aeb3b6d
Vojdan Bojcev <vbojcev@uwaterloo.ca>
2022-06-15 20:39:16 -0400

    help pls

--- sim.py
+++ sim.py
@@ -13,33 +13,26 @@ from matplotlib.pyplot import figure
 
 #figure(figsize=(8, 6), dpi=80)
 
+#enumeration for sensor type -> radius
 sensorType = {"A": [100, 300], "B": [70, 170], "C": [30, 65]}
 
 class Target:
     def __init__(self):
         self.location = [random.randint(0,500), random.randint(0,500)] 
-        self.k=0
-
-    def covInc():
-        k+=1
-
-    def covDec():
-        k-=1
+        self.k = 0
 
 targets = []
 
 class Sensor:
     def __init__(self, location, type):
-        self.location=location
-        self.type=type
-        self.range=sensorType[type][0]
-        self.cost=sensorType[type][1]
-        
-def checkCoverage(target, sensor):
-    if (sqrt(pow() + pow()) <= sensor.range):
-        target.covInc()
-    else:
-        target.covDec()
+        self.location = location
+        self.type = type
+        self.range = sensorType[type][0]
+        self.cost = sensorType[type][1]
+
+    def setType(self, type):
+        self.range = sensorType[type][0]
+        self.cost = sensorType[type][1]
 
 for i in range(17):
     targets.append(Target())
@@ -65,3 +58,68 @@ fig.set_size_inches(5, 5)
 ax.set(xlim=(0, 500), ylim=(0, 500))
 
 plt.show()
+
+initialLocations = []
+
+globalK = 3
+
+#initialize set of all 500x500 potential sites
+for i in range(500):
+    for j in range (500):
+        initialLocations.append([i, j])
+
+#do not allow sensor and target at the same site
+for target in targets:
+    if target.location in initialLocations:
+        initialLocations.remove(target.location)
+
+sensors = []
+
+numRemoved = 0
+
+print(initialLocations)
+
+for loc in initialLocations:
+    sensors.append(Sensor(loc, 'A'))
+
+#compute initial coverage for each target
+for sensor in sensors:
+    for target in targets:
+        if sqrt(pow(sensor.location[0] - target.location[0], 2) + pow(sensor.location[0] - target.location[0], 2)) <= sensor.range:
+            target.k += 1
+
+#see which sensors we can remove
+for sensor in sensors:
+    for target in targets:
+        if sqrt(pow(sensor.location[0] - target.location[0], 2) + pow(sensor.location[0] - target.location[0], 2)) <= sensor.range:
+            if target.k > globalK:
+                target.k -= 1
+                sensors.remove(sensor)
+                numRemoved += 1
+
+print("Removed in first sweep: ", numRemoved)
+numRemoved = 0
+
+#check if we can turn any sensors into type B
+for sensor in sensors:
+    for target in targets:
+        if sqrt(pow(sensor.location[0] - target.location[0], 2) + pow(sensor.location[0] - target.location[0], 2)) <= sensorType["B"][0]:
+            if target.k > globalK:
+                target.k -= 1
+                sensors.setType("B")
+                numRemoved += 1
+
+print("Changed to type B: ", numRemoved)
+numRemoved = 0
+
+#check if we can turn any sensors into type C
+for sensor in sensors:
+    for target in targets:
+        if sqrt(pow(sensor.location[0] - target.location[0], 2) + pow(sensor.location[0] - target.location[0], 2)) <= sensorType["C"][0]:
+            if target.k > globalK:
+                target.k -= 1
+                sensors.setType("C")
+                numRemoved += 1
+
+print("Changed to type B: ", numRemoved)
+numRemoved = 0
